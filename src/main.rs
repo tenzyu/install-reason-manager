cf13877621ed4d86102c760a23b6610640059a59
tenzyu <tenzyu.on@gmail.com>
2024-11-10 19:42:59 +0900

    second implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser, Subcommand};
 use colored::*;
 use dialoguer::{theme::ColorfulTheme, Input, Select};
 use serde::{Deserialize, Serialize};
@@ -8,19 +8,35 @@ use std::io;
 use std::path::PathBuf;
 use std::process::Command;
 
-const PROGRAM_NAME: &str = "install_reason_manager";
-const STATE_FILE: &str = "state.json";
+const PROGRAM_NAME: &str = "moree";
+const DEFAULT_STATE_FILE: &str = "state.json";
 
 #[derive(Parser)]
 #[command(name = PROGRAM_NAME)]
-#[command(about = "Manage explicitly installed packages interactively.")]
+#[command(about = "Manage packages and their explicit installation reasons")]
 struct Cli {
-    #[arg(long)]
-    list: bool,
-    #[arg(long)]
-    reinstall: bool,
-    // #[arg(long)]
-    // cleanup: bool,
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    #[arg(long, value_name = "path")]
+    data: Option<PathBuf>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        packages: Vec<String>,
+    },
+    Apply {
+        #[arg(long)]
+        with_install: bool,
+        #[arg(long)]
+        with_uninstall: bool,
+        #[arg(long)]
+        sync: bool,
+    },
+    Managed,
+    Unmanaged,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -31,53 +47,84 @@ struct PackageState {
 
 fn main() -> io::Result<()> {
     let args = Cli::parse();
-    let data_dir = get_data_dir()?;
-    let state_file_path = data_dir.join(STATE_FILE);
 
+    let state_file_path = get_state_file_path(&args.data)?;
     let mut package_states: HashMap<String, PackageState> = load_package_states(&state_file_path)?;
 
-    if args.list {
-        display_explicit_packages(&package_states);
-        return Ok(());
-    }
-
-    let output = Command::new("paru")
-        .arg("-Qe")
-        .output()
-        .expect("Failed to execute paru.");
-    let output_str = String::from_utf8_lossy(&output.stdout);
-
-    if args.reinstall {
-        reinstall_as_deps(&output_str, &package_states)?;
-        return Ok(());
-    }
-    // if args.cleanup {
-    // cleanup_packages(&output_str, &package_states)?;
-    // return Ok(());
-    // }
-
-    for line in output_str.lines() {
-        let package_name = line.split_whitespace().next().unwrap().to_string();
-        if let Some(state) = package_states.get(&package_name) {
-            if state.explicit {
-                continue;
+    match &args.command {
+        Some(Commands::Add { packages }) => {
+            if packages.is_empty() {
+                interactive_mode(&mut package_states)?;
+            } else {
+                add_packages(&mut package_states, packages)?;
             }
         }
-        handle_package_interactively(&mut package_states, &package_name)?;
+        Some(Commands::Apply {
+            with_install,
+            with_uninstall,
+            sync,
+        }) => {
+            if *sync || (*with_install && *with_uninstall) {
+                apply_changes(&package_states, true, true)?;
+            } else {
+                apply_changes(&package_states, *with_install, *with_uninstall)?;
+            }
+        }
+        Some(Commands::Managed) => display_managed_packages(&package_states),
+        Some(Commands::Unmanaged) => display_unmanaged_packages(&package_states)?,
+        None => {
+            println!("{}", Cli::command().render_long_help());
+        }
     }
 
     save_package_states(&state_file_path, &package_states)?;
-
     Ok(())
 }
 
-fn get_data_dir() -> io::Result<PathBuf> {
-    let data_dir = std::env::var("XDG_STATE_HOME")
+// NOTE: 参照じゃなくても良いかも
+fn get_custom_state_file_path(custom_path: &PathBuf) -> io::Result<PathBuf> {
+    // 指定されたパスがディレクトリでないか確認
+    if custom_path.is_dir() {
+        eprintln!("Error: The provided path is a directory, not a file.");
+        std::process::exit(1);
+    }
+
+    // ディレクトリがない場合は作成
+    if let Some(parent_dir) = custom_path.parent() {
+        fs::create_dir_all(parent_dir)?;
+    }
+
+    // ファイル名が .json で終わっているか確認
+    if custom_path.extension().and_then(|ext| ext.to_str()) != Some("json") {
+        println!("The provided path does not have a .json extension. Are you sure you want to use this path? (y/n)");
+        let confirmation: String = Input::<String>::new()
+            .interact_text()
+            .unwrap_or_default()
+            .to_lowercase();
+        if confirmation != "y" {
+            eprintln!("Operation canceled by the user.");
+            std::process::exit(1);
+        }
+    }
+
+    Ok(custom_path.clone())
+}
+
+fn get_default_state_file_path() -> io::Result<PathBuf> {
+    let data_dir = std::env::var("XDG_DATA_HOME")
         .map(PathBuf::from)
-        .unwrap_or_else(|_| dirs::home_dir().unwrap().join(".local/state"))
+        .unwrap_or_else(|_| dirs::home_dir().unwrap().join(".local/share"))
         .join(PROGRAM_NAME);
+
     fs::create_dir_all(&data_dir)?;
-    Ok(data_dir)
+    Ok(data_dir.join(DEFAULT_STATE_FILE))
+}
+
+fn get_state_file_path(custom_path: &Option<PathBuf>) -> io::Result<PathBuf> {
+    match custom_path {
+        Some(path) => get_custom_state_file_path(path),
+        None => get_default_state_file_path(),
+    }
 }
 
 fn load_package_states(file_path: &PathBuf) -> io::Result<HashMap<String, PackageState>> {
@@ -96,6 +143,74 @@ fn save_package_states(
     fs::write(file_path, data)
 }
 
+fn add_packages(
+    package_states: &mut HashMap<String, PackageState>,
+    packages: &[String],
+) -> io::Result<()> {
+    let installed_packages = Command::new("paru")
+        .arg("-Q")
+        .output()
+        .expect("Failed to execute paru -Q.");
+    let installed_list: Vec<String> = String::from_utf8_lossy(&installed_packages.stdout)
+        .lines()
+        .map(|line| line.split_whitespace().next().unwrap().to_string())
+        .collect();
+
+    let mut missing_packages = vec![];
+
+    for package in packages {
+        if installed_list.contains(package) {
+            handle_package_interactively(package_states, package)?;
+        } else {
+            missing_packages.push(package.clone());
+        }
+    }
+
+    if !missing_packages.is_empty() {
+        println!(
+            "Error: Some packages are not installed: {:?}",
+            missing_packages
+        );
+    }
+
+    Ok(())
+}
+
+fn interactive_mode(package_states: &mut HashMap<String, PackageState>) -> io::Result<()> {
+    let output = Command::new("paru")
+        .arg("-Qe")
+        .output()
+        .expect("Failed to execute paru -Qe.");
+    let output_str = String::from_utf8_lossy(&output.stdout);
+
+    for line in output_str.lines() {
+        let package_name = line.split_whitespace().next().unwrap().to_string();
+        if let Some(state) = package_states.get(&package_name) {
+            if state.explicit {
+                continue;
+            }
+        }
+        if handle_package_interactively(package_states, &package_name).is_err() {
+            loop {
+                println!("Would you like to save your progress? (Y/n)");
+                let save: String = Input::<String>::new()
+                    .interact_text()
+                    .unwrap_or_default()
+                    .to_lowercase();
+                if save == "y" || save == "n" {
+                    if save == "y" {
+                        break;
+                    }
+                } else {
+                    println!("Invalid input. Please enter 'y' or 'n'.");
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn handle_package_interactively(
     package_states: &mut HashMap<String, PackageState>,
     package_name: &str,
@@ -104,7 +219,7 @@ fn handle_package_interactively(
     // TODO: もっと小さい情報を見せる
     display_package_details(package_name);
 
-    let options = vec!["Yes (y)", "No (n)", "Skip (s)", "Details (d)"];
+    let options = vec!["Yes (y)", "No (n)", "Skip (s)", "Details (d)", "Quit (q)"];
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Did you explicitly install this package?")
         .items(&options)
@@ -142,6 +257,7 @@ fn handle_package_interactively(
             display_package_details(package_name);
             handle_package_interactively(package_states, package_name)?;
         }
+        'q' | 'Q' => return Err(io::Error::new(io::ErrorKind::Other, "Quit")),
         _ => {}
     }
 
@@ -171,8 +287,8 @@ fn display_package_details(package_name: &str) {
     // println!("{}", pactree_output_str);
 }
 
-fn display_explicit_packages(package_states: &HashMap<String, PackageState>) {
-    println!("{}", "Explicitly Installed Packages:".bold().green());
+fn display_managed_packages(package_states: &HashMap<String, PackageState>) {
+    println!("{}", "Managed Packages:".bold().green());
     for (name, state) in package_states {
         if state.explicit {
             let memo = state.memo.as_deref().unwrap_or("No memo provided.");
@@ -181,62 +297,62 @@ fn display_explicit_packages(package_states: &HashMap<String, PackageState>) {
     }
 }
 
-fn reinstall_as_deps(
-    installed_output: &str,
-    package_states: &HashMap<String, PackageState>,
-) -> io::Result<()> {
-    let mut to_reinstall = Vec::new();
-    for line in installed_output.lines() {
+fn display_unmanaged_packages(package_states: &HashMap<String, PackageState>) -> io::Result<()> {
+    let output = Command::new("paru")
+        .arg("-Qe")
+        .output()
+        .expect("Failed to execute paru -Qe.");
+    let output_str = String::from_utf8_lossy(&output.stdout);
+
+    println!("{}", "Unmanaged Packages:".bold().red());
+    for line in output_str.lines() {
         let package_name = line.split_whitespace().next().unwrap().to_string();
-        if let Some(state) = package_states.get(&package_name) {
-            if !state.explicit {
-                to_reinstall.push(package_name);
-            }
+        if !package_states.contains_key(&package_name) {
+            println!("{}", package_name.bold());
         }
     }
 
-    if !to_reinstall.is_empty() {
-        println!(
-            "{}",
-            format!("Re-installing as deps {} packages...", to_reinstall.len())
-                .bold()
-                .red()
-        );
-        Command::new("paru")
-            .arg("-S")
-            .arg("--asdeps")
-            .args(&to_reinstall)
-            .status()
-            .expect("Failed to execute paru -S --asdeps.");
+    Ok(())
+}
+
+fn apply_changes(
+    package_states: &HashMap<String, PackageState>,
+    with_install: bool,
+    with_uninstall: bool,
+) -> io::Result<()> {
+    if with_install {
+        for (package, state) in package_states {
+            if state.explicit && !is_installed(package) {
+                Command::new("paru")
+                    .arg("-S")
+                    .arg(package)
+                    .status()
+                    .expect("Failed to install package.");
+            }
+        }
+    }
+    if with_uninstall {
+        let output = Command::new("paru")
+            .arg("-Qe")
+            .output()
+            .expect("Failed to execute paru -Qe.");
+        let output_str = String::from_utf8_lossy(&output.stdout);
+
+        for line in output_str.lines() {
+            let package_name = line.split_whitespace().next().unwrap();
+            if !package_states.contains_key(package_name) {
+                Command::new("paru")
+                    .arg("-R")
+                    .arg(package_name)
+                    .status()
+                    .expect("Failed to uninstall package.");
+            }
+        }
     }
     Ok(())
 }
-// fn cleanup_packages(
-//     installed_output: &str,
-//     package_states: &HashMap<String, PackageState>,
-// ) -> io::Result<()> {
-//     let mut to_remove = Vec::new();
-//     for line in installed_output.lines() {
-//         let package_name = line.split_whitespace().next().unwrap().to_string();
-//         if let Some(state) = package_states.get(&package_name) {
-//             if !state.explicit {
-//                 to_remove.push(package_name);
-//             }
-//         }
-//     }
 
-//     if !to_remove.is_empty() {
-//         println!(
-//             "{}",
-//             format!("Cleaning up {} packages...", to_remove.len())
-//                 .bold()
-//                 .red()
-//         );
-//         Command::new("paru")
-//             .arg("-Rns")
-//             .args(&to_remove)
-//             .status()
-//             .expect("Failed to execute paru -R.");
-//     }
-//     Ok(())
-// }
+fn is_installed(package: &str) -> bool {
+    let status = Command::new("paru").arg("-Qi").arg(package).status();
+    status.map(|s| s.success()).unwrap_or(false)
+}
